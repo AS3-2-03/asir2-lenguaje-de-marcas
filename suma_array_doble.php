e1c597b890ac5844dcd1b26fd35cdda3cd5d7939
AS3-2-03 <AS3-2-03@uni.lan>
2019-10-09 12:05:54 +0200

    a

---
+++ suma_array_doble.php
@@ -0,0 +1,18 @@
+<?php
+
+$vv = [
+    [1,2,3],
+    [4,5,6]
+];
+
+
+
+$suma = 0;
+for ($x=0; $x <=6; $x++) {
+    // echo "El número es $x <br>";
+    $suma = $suma + $vv;
+    
+}
+// echo "La suma es = " . array_sum($v) . "\n";
+
+    echo "batura : " . $suma;
