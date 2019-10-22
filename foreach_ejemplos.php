8f831bcbcea0a03acce0b77588ff6223fa1e7fe8
AS3-2-03 <AS3-2-03@uni.lan>
2019-10-22 08:53:34 +0200

    a

--- foreach_ejemplos.php
+++ foreach_ejemplos.php
@@ -1,13 +1,19 @@
 <?php
 
+$v = [1,2,3];
+
+
+
+
+foreach ($v as $x) {
+    echo "$x <br>";
+    //$suma = $suma + $x;
+}
+
+echo "<br>-------------------------<br>";
+
+
 $v = ["juan","adrian","marc"];
-// $suma = 0;
-
-// $suma = $v[0] + $v[1] + $v[2];
-// echo "batura : " . $suma;
-
-
-$x = 0;
 
 foreach ($v as $x) {
     echo "$x <br>";
