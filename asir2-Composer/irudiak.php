e870de70d1f90f81dcb64f0724249898ec345c2d
Adrian <AS3-2-03.19@uni.eus>
2019-12-17 09:17:20 +0100

    a

--- asir2-Composer/irudiak.php
+++ asir2-Composer/irudiak.php
@@ -1,35 +1,37 @@
 <?php
-
-
-$image = new Imagick();
-
-
 // include composer autoload
-// require __DIR__ . '/vendor/autoload.php';
-
+//require __DIR__ . '/vendor/autoload.php';
 require 'vendor/autoload.php';
-
+// http://image.intervention.io/
 // import the Intervention Image Manager Class
-    use Intervention\Image\ImageManager;
-
-    //use ImageManager;
-
-
-// create an image manager instance with favored driver
-    $manager = new ImageManager(array('driver' => 'imagick'));
-
-// to finally create image instances
-    $image = $manager->make('images/paisaje1.jpg')->resize(300, 200);
-
-
-    //new \Intervention\Image\ImageManager(array('driver' => 'gd'));
-
-//use Intervention\Image\ImageManagerStatic;
-//use Imagick;
-    
-    $Image::configure(['driver' => 'imagick']);
-
-
-//echo "kaixo";
-
-?>
+use Intervention\Image\ImageManagerStatic as Image;
+// configure with favored image driver (gd by default)
+Image::configure(array('driver' => 'imagick'));
+// and you are ready to go ...
+$image = Image::make('images/paisaje1.jpg')->resize(300, 200);
+//// finally we save the image as a new file
+//$image->save('eibar2.jpg');
+//
+$image->encode('png');
+$type = 'png';
+$base64 = 'data:image/' . $type . ';base64,' . base64_encode($image);
+echo "<img src=$base64>";
+//*********** */
+// resize the image to a width of 300 and constrain aspect ratio (auto height)
+$image = Image::make('images/paisaje1.jpg');
+$image->resize(300, null, function ($constraint) {
+    $constraint->aspectRatio();
+});
+$image->greyscale();
+$image->encode('png');
+$type = 'png';
+$base64 = 'data:image/' . $type . ';base64,' . base64_encode($image);
+echo "<img src=$base64>";
+//*********** */
+$image = Image::make('images/paisaje1.jpg');
+// rotate image 45 degrees clockwise
+$image->rotate(-45);
+$image->encode('png');
+$type = 'png';
+$base64 = 'data:image/' . $type . ';base64,' . base64_encode($image);
+echo "<img src=$base64>";
