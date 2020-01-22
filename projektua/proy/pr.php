046df2e2363f4b0c2f319ffdd90570f674cd67cc
AS3-2-03 <AS3-2-03.19@uni.eus>
2020-01-22 09:46:47 +0100

    a

--- projektua/proy/pr.php
+++ projektua/proy/pr.php
@@ -5,7 +5,7 @@ require '../vendor/autoload.php';
 use DmitryIvanov\DarkSkyApi\DarkSkyApi;
 
 $forecast = (new DarkSkyApi('e74b990fb81cf9ad34a2c13c4d21604a'))
-    ->location(53.4809494, -2.2374301)
+    ->location( -15.5000000, 28.0000000)
     ->units('si')
     ->language('es')
     ->forecast();
